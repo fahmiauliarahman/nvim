8109f8bf2ce4da4c88dfe52de3818fd1b046bfb5
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2025-11-22 21:46:43 +0700

    Add completion and theme configurations
    - Updated `lazy-lock.json` with new commits for several plugins, including `conform.nvim`, `mini.ai`, `nvim-lint`, and `nvim-lspconfig`.
    - Added new Lua files for completion and theme configurations, integrating `nvim-cmp` with snippets support and setting up the `gruvbox` theme with specific options.

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "hard", -- make the palette darker than the default
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
