9905734826ce101a06bf64bf6a6074f3cdf55d3e
Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>
2026-01-04 17:13:28 +0700

    feat(goctl): add goctl.nvim plugin with format on save and keymaps
    Signed-off-by: Fahmi Aulia Rahman <rahman.fahmiaulia@gmail.com>

---
+++ lua/plugins/goctl.lua
@@ -0,0 +1,10 @@
+return {
+  "fahmiauliarahman/goctl.nvim",
+  ft = { "goctl", "api" },
+  opts = {
+    format_on_save = true,
+    goctl_path = "goctl",
+    enable_snippets = true,
+    enable_keymaps = true,
+  },
+}
